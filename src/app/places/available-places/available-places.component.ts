77c854315cebbebe4d5e246dc5c1c5bbedd0363f
SwadhiSankar <swadhis14@gmail.com>
2024-07-31 14:56:21 +0530

    Get API

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
@@ -11,10 +11,22 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './available-places.component.html',
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
- 
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   private httpClient = inject(HttpClient);
-  
+  private destroyRef = inject(DestroyRef);
+  // constructor(private httpClient: HttpClient) {}
+  ngOnInit() {
+    const Subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places')
+      .subscribe({
+        next: (resData) => {
+          console.log(resData.places);
+        },
+      });
+    this.destroyRef.onDestroy(() => {
+      Subscription.unsubscribe();
+    });
+  }
 }
