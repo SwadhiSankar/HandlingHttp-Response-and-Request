78aa49b89fe88cfc4986cba2c13fa1e638c5e625
SwadhiSankar <swadhis14@gmail.com>
2024-08-02 16:50:16 +0530

    Using get API to fetch the selected favrt places

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-user-places',
@@ -11,4 +14,39 @@ import { PlacesComponent } from '../places.component';
   imports: [PlacesContainerComponent, PlacesComponent],
 })
 export class UserPlacesComponent {
+  places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+  ngOnInit() {
+    this.isFetching.set(true);
+    const Subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/user-places')
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(() => new Error('Fav places issue while feching.'));
+        })
+      )
+      .subscribe({
+        next: (places) => {
+          console.log(places);
+          console.log(places);
+          this.places.set(places);
+        },
+        error: (error: Error) => {
+          console.log(error);
+          this.error.set(error.message);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        },
+      });
+    this.destroyRef.onDestroy(() => {
+      Subscription.unsubscribe();
+    });
+  }
+
 }
